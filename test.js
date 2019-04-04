99d993e0fd87bd814d085e12b7f9169c2ba8d494
johannpickard <johannpickard@gmail.com>
2019-04-04 19:47:48 +0000

    a bunch

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-var expect = require('expect.js');
+
+
+
+// I need to declare a graph ... the one below shoudl do. This will be unique for my buidling
+
+            var graph = {
+                a: {b: 10, c: 100, d: 1},
+                b: {c: 10},
+                d: {b: 1, e: 1},
+                e: {f: 1},
+                f: {c: 1},
+                g: {b: 1}
+            };
